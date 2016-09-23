3727c8f1d401187c969f77fe689e3e1e6b5e80d0
Nabina Shahi <nabina.shahi@gmail.com>
2016-09-23 13:40:53 -0500

    nabina did this

--- src/main/java/org/shopping/app/OnlineShoppingApplication.java
+++ src/main/java/org/shopping/app/OnlineShoppingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineShoppingApplication {
 
 	public static void main(String[] args) {
+		System.out.println("nabina");
 		SpringApplication.run(OnlineShoppingApplication.class, args);
 	}
 }
