8780bedd8f2197cb32c31605f4d300af4be5463c
M.elkhouly <mahmoud_m.elkhouly@yahoo.com>
2016-09-23 13:50:19 -0500

    1:50

--- src/main/java/org/shopping/app/OnlineShoppingApplication.java
+++ src/main/java/org/shopping/app/OnlineShoppingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineShoppingApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test mahmoud  again");
+		System.out.println("test mahmoud  149");
 		SpringApplication.run(OnlineShoppingApplication.class, args);
 	}
 }
