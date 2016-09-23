cc70c5e7626cb8614f526f829736c9dcdd82013f
mahmoud-PC\mahmoud <mahmoud.elkholyy@gmail.com>
2016-09-23 13:21:46 -0500

    commit by mahmoud

--- src/main/java/org/shopping/app/OnlineShoppingApplication.java
+++ src/main/java/org/shopping/app/OnlineShoppingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineShoppingApplication {
 
 	public static void main(String[] args) {
+		System.out.println("test mahmoud");
 		SpringApplication.run(OnlineShoppingApplication.class, args);
 	}
 }
